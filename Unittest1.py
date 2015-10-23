fc961bdc1f37ce891fa2939dbe89a39529f28554
vikramuk <vikramuk@gmail.com>
2015-10-23 09:17:03 +0530

    Create Unittest1.py

---
+++ Unittest1.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        # import class and prepare everything here.        
+        pass
+    
+def test(self):
+    # place your test case here
+    a = 1
+    self.assertEqual(a, 1)
+
